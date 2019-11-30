dfd3b821b0dd0aaf9db7d3c7d808547023309165
Shaun Campbell <shaun@shauntc.com>
2019-11-30 11:33:41 -0800

    Add saving the image buffer

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,37 @@ mod scene;
 mod rendering;
 mod point;
 
+use rendering::{render};
+use image::{DynamicImage};
+use point::Point;
+use scene::{Sphere, Scene, Color};
+use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
+    let scene = Scene {
+        width: 800,
+        height: 600,
+        fov: 90.0,
+        sphere: Sphere {
+            center: Point {
+                x: 0.0,
+                y: 0.0,
+                z: -5.0,
+            },
+            radius: 1.0,
+            color: Color {
+                red: 0.4,
+                green: 1.0,
+                blue: 0.4,
+            },
+        },
+    };
+
+    let img = render(&scene);
+    let path = Path::new("./test.png"); // TODO: Change .gitignore when this is removed
+    if img.save(path).is_ok() {
+        println!("Image Saved to {:?}", path);
+    } else {
+        println!("Failed to save image");
+    }
 }
