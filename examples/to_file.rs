e10e508b152ac87ebaa755909f082781076ce28b
Shaun Campbell <shaun@shauntc.com>
2020-01-01 08:34:37 -0800

    update sample scene with reflective materials

--- examples/to_file.rs
+++ examples/to_file.rs
@@ -15,6 +15,7 @@ fn main() {
                 height: 600,
                 fov: 90.0,
                 shadow_bias: 1e-12,
+                max_recursion_depth: 4
             },
             elements: vec![
                 Element::Plane(Plane {
@@ -22,33 +23,44 @@ fn main() {
                     normal: Vector3 {x: 0.0, y: -1.0, z: 0.0},
                     material: Material {
                         color: Coloration::Color(Color {red: 0.2, green: 0.01, blue: 0.8}),
-                        albedo: 0.12
+                        albedo: 0.12,
+                        surface: SurfaceType::Diffuse
                     }
                 }), 
                 Element::Sphere(Sphere {
-                    center: Point {x: -1.0, y: 1.0, z: -3.0},
+                    center: Point {x: -1.5, y: 1.0, z: -3.0},
                     radius: 1.0,
                     material: Material {
                         color: Coloration::Color(Color {red: 1.0, green: 0.01, blue: 0.01}),
-                        albedo: 0.04
+                        albedo: 0.04,
+                        surface: SurfaceType::Diffuse
                     }
                 }),
                 Element::Sphere(Sphere {
-                    center: Point {x: 1.0, y: -1.5, z: -5.0},
+                    center: Point {x: 2.0, y: -1.5, z: -5.0},
                     radius: 1.5,
                     material: Material {
                         color: Coloration::Texture(texture),
-                        // color: Coloration::Color(Color {red: 0.01, green: 1.0, blue: 0.01}),
-                        albedo: 0.05
+                        albedo: 0.05,
+                        surface: SurfaceType::Diffuse
                     }
                 }),
                 Element::Sphere(Sphere {
                     center: Point {x: 0.0, y: 2.0, z: -5.0},
                     radius: 1.0,
                     material: Material {
-                        // color: Coloration::Texture(texture),
                         color: Coloration::Color(Color {red: 0.01, green: 0.01, blue: 1.0}),
-                        albedo: 0.20
+                        albedo: 0.20,
+                        surface: SurfaceType::Reflective { reflectivity: 0.75 }
+                    }
+                }),
+                Element::Sphere(Sphere {
+                    center: Point {x: -1.5, y: -1.0, z: -7.0},
+                    radius: 2.0,
+                    material: Material {
+                        color: Coloration::Color(Color {red: 1.0, green: 1.0, blue: 1.0}),
+                        albedo: 0.20,
+                        surface: SurfaceType::Reflective { reflectivity: 1.0 }
                     }
                 }),
             ],
@@ -57,7 +69,7 @@ fn main() {
                 Light::Directional(DirectionalLight {
                     direction: Vector3 {x: -1.0, y:-1.0, z: -1.5},
                     color: Color {red: 1.0, green: 1.0, blue: 1.0},
-                    intensity: 5.0
+                    intensity: 10.0
                 }),
                 Light::Spherical(SphericalLight {
                     position: Point {x: 0.0, y:0.0, z: -3.0},
